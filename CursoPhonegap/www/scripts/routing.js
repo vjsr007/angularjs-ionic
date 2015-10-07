ae12d04a9eca1044411579df1668b5d751523024
vjsr007 <vjsanchezr@hotmail.com>
2015-10-07 01:49:40 -0500

    ok

--- CursoPhonegap/www/scripts/routing.js
+++ CursoPhonegap/www/scripts/routing.js
@@ -1,21 +1,17 @@
 ﻿app.Angular.config(['$routeProvider', '$controllerProvider',
     function($routeProvider, $controllerProvider) {
-    // remember mentioned function for later use
         app.Angular.registerCtrl = $controllerProvider.register;
-            //your routes
-        // route for the home page
+
         $routeProvider.when('/', {
             templateUrl: 'views/home.html',
             controller: 'mainController'
         });
 
-        // route for the about page
         $routeProvider.when('/about', {
             templateUrl: 'views/about.html',
             controller: 'aboutController'
         });
 
-        // route for the contact page
         $routeProvider.when('/contact', {
             templateUrl: 'views/contact.html',
             controller: 'contactController'
