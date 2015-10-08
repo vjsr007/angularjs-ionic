f2e7d50df560fb5d72814a86b23e5da9bd96f773
vjsr007 <vjsanchezr@hotmail.com>
2015-10-08 00:19:52 -0500

    ok

--- CursoPhonegap/www/scripts/controller/mainController.js
+++ CursoPhonegap/www/scripts/controller/mainController.js
@@ -1,3 +1,5 @@
 ﻿app.Angular.registerCtrl('mainController', function ($scope) {
-    $scope.message = 'Bienvenido al Curso de Phonegap Cordova';
+    var deviceID = device.uuid;
+
+    $scope.message = 'Bienvenido al Curso de Phonegap Cordova\n' + deviceID;
 });
--- CursoPhonegap/www/scripts/controller/ubicacionController.js
+++ CursoPhonegap/www/scripts/controller/ubicacionController.js
@@ -1,99 +1,106 @@
 ﻿app.Angular.registerCtrl('ubicacionController', function ($scope) {
-    $scope.message = 'Mi Ubicación';
+    $scope.message = 'Arrastra el icono a tu destino';
 
-    var init = function () {
+    var directionsDisplay;
+    var directionsService = new google.maps.DirectionsService();
+    var map;
+    var markerDrag;
 
-        var directionsDisplay;
-        var directionsService = new google.maps.DirectionsService();
-        var map;
-        var markerDrag;
+    $scope.IniciarMapa = function (lat, lon) {
+        var point = new google.maps.LatLng(lat, lon);
+        var pointDrag = new google.maps.LatLng(lat, lon);
 
-        $scope.IniciarMapa = function() {
-            var point = new google.maps.LatLng(25.690081924266817, -100.26957783312992);
-            var pointDrag = new google.maps.LatLng(25.68, -100.27);
+        directionsDisplay = new google.maps.DirectionsRenderer({
+            draggable: true
+        });
 
-            directionsDisplay = new google.maps.DirectionsRenderer({
-                draggable: true
+        var myOptions = {
+            zoom: 14,
+            center: point,
+            mapTypeId: google.maps.MapTypeId.ROADMAP,
+            streetViewControl: true
+        };
+
+        map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+
+        var marker = new google.maps.Marker({
+            position: point, clickable: true,
+            map: map,
+            title: "Mi Ubicación"
+        });
+
+        markerDrag = new google.maps.Marker({
+            position: pointDrag, clickable: true,
+            draggable: true,
+            map: map,
+            title: "Arrastrame",
+            icon: "images/dragme.png"
+        });
+
+        var infowindow = new google.maps.InfoWindow({
+            width: 400,
+            height: 400,
+            disableAutoPan: false
+        });
+        infowindow.setContent("Arrástrame a tu destino");
+        infowindow.open(map, markerDrag);
+
+        google.maps.event.addListener(markerDrag, 'dragend', function (evt) {
+            $scope.calcRoute(lat, lon);
+        });
+
+        try {
+            var weatherLayer = new google.maps.weather.WeatherLayer({
+                temperatureUnits: google.maps.weather.TemperatureUnit.CELSIUS
             });
+            weatherLayer.setMap(map);
 
-            var myOptions = {
-                zoom: 14,
-                center: point,
-                mapTypeId: google.maps.MapTypeId.ROADMAP,
-                streetViewControl: true
-            };
-
-            map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
-
-            var marker = new google.maps.Marker({
-                position: point, clickable: true,
-                map: map,
-                title: "Mi Ubicación"
-            });
-
-            markerDrag = new google.maps.Marker({
-                position: pointDrag, clickable: true,
-                draggable: true,
-                map: map,
-                title: "Arrastrame",
-                icon: "images/dragme.png"
-            });
-
-            var infowindow = new google.maps.InfoWindow({
-                width: 400,
-                height: 400,
-                disableAutoPan: false
-            });
-            infowindow.setContent("Arrástrame a tu destino");
-            infowindow.open(map, markerDrag);
-
-            google.maps.event.addListener(markerDrag, 'dragend', function (evt) {
-                $scope.calcRoute();
-            });
-
-            try {
-                var weatherLayer = new google.maps.weather.WeatherLayer({
-                    temperatureUnits: google.maps.weather.TemperatureUnit.CELSIUS
-                });
-                weatherLayer.setMap(map);
-
-                var cloudLayer = new google.maps.weather.CloudLayer();
-                cloudLayer.setMap(map);
-            }
-            catch (e) {
-            }
+            var cloudLayer = new google.maps.weather.CloudLayer();
+            cloudLayer.setMap(map);
         }
-
-        $scope.calcRoute = function() {
-            var start = new google.maps.LatLng(25.690081924266817, -100.26957783312992);
-            var end = markerDrag.getPosition();
-
-            var wayPoints = new Array;
-            var Origins = new Array;
-            var Destinations = new Array;
-
-            Origins.push(start);
-            Destinations.push(end);
-
-            var request = {
-                origin: start,
-                destination: end,
-                waypoints: wayPoints,
-                travelMode: google.maps.TravelMode.DRIVING,
-                unitSystem: google.maps.UnitSystem.METRIC
-            };
-            directionsService.route(request, function (result, status) {
-                if (status == google.maps.DirectionsStatus.OK) {
-                    directionsDisplay.setDirections(result);
-                }
-            });
-
-            directionsDisplay.setMap(map);
-            directionsDisplay.setOptions({ suppressMarkers: true });
+        catch (e) {
         }
-
-        $scope.IniciarMapa();
     }
 
-    $(init);
+    $scope.calcRoute = function (lat, lon) {
+        var start = new google.maps.LatLng(lat, lon);
+        var end = markerDrag.getPosition();
+
+        var wayPoints = new Array;
+        var Origins = new Array;
+        var Destinations = new Array;
+
+        Origins.push(start);
+        Destinations.push(end);
+
+        var request = {
+            origin: start,
+            destination: end,
+            waypoints: wayPoints,
+            travelMode: google.maps.TravelMode.DRIVING,
+            unitSystem: google.maps.UnitSystem.METRIC
+        };
+        directionsService.route(request, function (result, status) {
+            if (status == google.maps.DirectionsStatus.OK) {
+                directionsDisplay.setDirections(result);
+            }
+        });
+
+        directionsDisplay.setMap(map);
+        directionsDisplay.setOptions({ suppressMarkers: true });
+    }
+
+    document.addEventListener("deviceready", onDeviceReady, false); 
+             
+    function onDeviceReady() { 
+        navigator.geolocation.getCurrentPosition(onSuccess, onError); 
+    } 
+             
+    function onSuccess(position) { 
+        $scope.IniciarMapa(position.coords.latitude, position.coords.longitude);
+    } 
+
+    function onError(error) { 
+        alert('Código: '    + error.code    + '\n' + 'Mensaje: ' + error.message + '\n'); 
+    }
 });
