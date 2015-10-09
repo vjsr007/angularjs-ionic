843abf516c8dc4752831daefff60eb1f0adb93cd
vjsr007 <vjsanchezr@hotmail.com>
2015-10-08 21:19:45 -0500

    ok

--- CursoPhonegap/www/scripts/controller/contactController.js
+++ CursoPhonegap/www/scripts/controller/contactController.js
@@ -1,3 +1,36 @@
 ﻿app.Angular.registerCtrl('contactController', function ($scope) {
-    $scope.message = 'Soy una prueba.';
+
+    $scope.message = "";
+
+    //document.addEventListener('deviceready', onDeviceReady, false);
+
+    //var onDeviceReady = function () {
+    //    init();
+    //};
+
+    //var onPause = function () {
+
+    //};
+
+    //var onResume = function () {
+    //};
+
+    //var init = function () {
+    //    $scope.message = 'Lista de Contactos.';
+
+    //    document.addEventListener('pause', onPause, false);
+    //    document.addEventListener('resume', onResume, false);
+
+    //    buscarContactos();
+    //}
+
+    //var buscarContactos = function () {
+    //    var options = new ContactFindOptions();
+    //    options.filter = "";
+    //    options.multiple = true;
+    //    options.desiredFields = [navigator.contacts.fieldType.id];
+    //    options.hasPhoneNumber = true;
+    //    var fields = [navigator.contacts.fieldType.displayName, navigator.contacts.fieldType.name];
+    //    navigator.contacts.find(fields, onSuccess, onError, options);
+    //}
 });
--- CursoPhonegap/www/scripts/controller/mainController.js
+++ CursoPhonegap/www/scripts/controller/mainController.js
@@ -1,5 +1,27 @@
 ﻿app.Angular.registerCtrl('mainController', function ($scope) {
-    var deviceID = device.uuid;
+    $scope.message = "";
 
-    $scope.message = 'Bienvenido al Curso de Phonegap Cordova\n' + deviceID;
+    //document.addEventListener("deviceready", onDeviceReady, false);
+
+    //var onDeviceReady = function () {
+    //    init();
+    //};
+
+    //var onPause = function () {
+
+    //};
+
+    //var onResume = function () {
+
+    //};
+
+    //var init = function () {
+    //    var deviceID = device.uuid;
+
+    //    $scope.message = 'Bienvenido al Curso de Phonegap Cordova\nUUID: ' + deviceID;
+
+    //    document.addEventListener('pause', onPause, false);
+    //    document.addEventListener('resume', onResume, false);
+    //}
+
 });
