8ea26b7a72948588b421d8f55d4289468a48aa1e
vjsr007 <vjsanchezr@hotmail.com>
2015-10-11 21:33:38 -0500

    ok

--- CursoPhonegap/www/scripts/controller/aboutController.js
+++ CursoPhonegap/www/scripts/controller/aboutController.js
@@ -1,4 +1,30 @@
 ﻿//@ sourceURL=aboutController.js
 app.Angular.registerCtrl('aboutController', function ($scope, $ionicSlideBoxDelegate) {
 
+    $scope.Acceleration;
+    $scope.Compass;
+
+    function onSuccess(acceleration) {
+        $scope.Acceleration = acceleration;
+    };
+
+    function onError() {
+        alert('onError!');
+    };
+
+    function onSuccessCompass(heading) {
+        $scope.Compass = heading;
+    };
+
+    function onDeviceReady() {
+        var options = { frequency: 1000 };  // Update every 3 seconds
+
+        var watchID = navigator.accelerometer.watchAcceleration(onSuccess, onError, options);
+
+        var watchCompassID = navigator.compass.watchHeading(onSuccessCompass, onError, options);
+    }
+
+    document.addEventListener("deviceready", onDeviceReady, false);
+
+
 });
