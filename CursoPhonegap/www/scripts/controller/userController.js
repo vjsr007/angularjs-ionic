e2254457795755eb9898b2a6243ede9283ef4b15
vjsr007 <vjsanchezr@hotmail.com>
2016-08-17 23:42:33 -0500

    OK

--- CursoPhonegap/www/scripts/controller/userController.js
+++ CursoPhonegap/www/scripts/controller/userController.js
@@ -7,9 +7,9 @@ app.Angular.registerCtrl('userController', function ($scope, $ionicSlideBoxDeleg
 
         var User = new DatabaseService.User;
 
-        User.UserName = $scope.UserName;
-        User.Name = $scope.Name;
-        User.Active = $scope.Active;
+        User.UserName($scope.UserName);
+        User.Name($scope.Name);
+        User.Active($scope.Active);
 
         DatabaseService.addRow(User);
     }
