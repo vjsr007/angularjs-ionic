ae41b5c21c5739692a901d2de7837311740da9ff
vjsr007 <vjsanchezr@hotmail.com>
2016-08-17 23:46:40 -0500

    ok

--- CursoPhonegap/www/scripts/controller/barcodeController.js
+++ CursoPhonegap/www/scripts/controller/barcodeController.js
@@ -15,10 +15,10 @@ app.Angular.registerCtrl('barcodeController', function ($scope, $ionicSlideBoxDe
                alert("Scanning failed: " + error);
            },
            {
-               "preferFrontCamera": true, // iOS and Android
+               "preferFrontCamera": false, // iOS and Android
                "showFlipCameraButton": true, // iOS and Android
                "prompt": "Place a barcode inside the scan area", // supported on Android only
-               "formats": "QR_CODE,PDF_417", // default: all but PDF_417 and RSS_EXPANDED
+               //"formats": "QR_CODE,PDF_417", // default: all but PDF_417 and RSS_EXPANDED
                "orientation": "landscape" // Android only (portrait|landscape), default unset so it rotates with the device
            }
         );
