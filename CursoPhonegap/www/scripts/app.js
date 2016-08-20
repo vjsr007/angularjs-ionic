389ab4008aa7bd7daa85a68b0e4227667f2815a8
vjsr007 <vjsanchezr@hotmail.com>
2016-08-20 14:47:22 -0500

    OK

--- CursoPhonegap/www/scripts/app.js
+++ CursoPhonegap/www/scripts/app.js
@@ -4,41 +4,18 @@
 
     self.emulated = window.parent && window.parent.ripple;
 
-    var init = function () {
+    self.Angular = angular
+        .module("CursoPhonegap", ['ngRoute', 'ionic', 'ngAnimate'])
+        .controller('appController', function ($scope, $ionicSideMenuDelegate) {
 
+            $scope.toggleLeft = function () {
+                $ionicSideMenuDelegate.toggleLeft();
+            };
+
+        });
+
+    window.onerror = function (e) {
+        alert("Error de Aplicación\n" + (e.message ? e.message : e));
     };
-
-    var initAngular = function () {
-        self.Angular = angular
-            .module("CursoPhonegap", ['ngRoute', 'ionic','ngAnimate'])
-            .controller('appController', function ($scope, $ionicSideMenuDelegate) {
-
-                $scope.toggleLeft = function () {
-                    $ionicSideMenuDelegate.toggleLeft();
-                };
-
-            });
-    }
-
-    var handledError = function () {
-        window.onerror = function (e) {
-            alert("Error de Aplicación\n" + (e.message?e.message:e));
-        };
-    };
-
-    var load = function () {
-        handledError();
-
-        //<summary>Cargar al momento</summary>
-        $(init);
-
-        initAngular();
-    };
-
-    var onError = function () {
-
-    };
-
-    load();
 
 })(app);
